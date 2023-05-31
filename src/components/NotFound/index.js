05900f48a9547b891953dd634e1086dd0a6c8384
pavani1107 <kallepavani@gmail.com>
2023-05-31 15:05:58 +0000

    tech era

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+
+const NotFound = () => (
+  <>
+    <Header />
+    <div className="not-found-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/tech-era/not-found-img.png"
+        className="not-found-image"
+        alt="not found"
+      />
+      <h1 className="not-found">Page Not Found</h1>
+      <p>We are sorry, the page you requested could not be found</p>
+    </div>
+  </>
+)
+export default NotFound
